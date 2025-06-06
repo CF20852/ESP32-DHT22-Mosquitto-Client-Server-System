318430c85dda4a252a06e7511f4f49ec7359c53d
CF20852 <CF20852@users.noreply.github.com>
2025-06-06 10:49:31 -0700

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,10 +1,10 @@
 // WiFi credentials
-const char* ssid = "22rv1";
-const char* password = "f%7Wbpahj*/8sU8![8qd7K8v#TV?jG%)";
+const char* ssid = <"your Wi-Fi SSID">;
+const char* password = <"your Wi-Fi password">;
 
 // MQTT Broker settings
 //const char* mqtt_server = "test.mosquitto.org";
-const char* mqtt_server = "192.168.1.237";  // Replace with your Raspberry Pi IP
+const char* mqtt_server = <"192.168.1.2">;  // Replace with your Raspberry Pi IP
 const int mqtt_port = 1883;
 const char* mqtt_user = "";  // Leave empty if no authentication
 const char* mqtt_pass = "";  // Leave empty if no authentication
